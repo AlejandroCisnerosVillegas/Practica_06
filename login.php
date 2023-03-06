86be2ce26fed19357d6ea68705cb229969752223
Alejandro Cisneros Villegas <125778068+AlejandroCisnerosVillegas@users.noreply.github.com>
2023-03-06 13:22:05 -0600

    Update login.php

--- login.php
+++ login.php
@@ -1,9 +1,9 @@
 <?php
 
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass = "";
-$dbname = "general";
+$dbhost = "IP_Address";
+$dbuser = "User";
+$dbpass = "Password";
+$dbname = "DataBase_Name";
 
 $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 if (!$conn) 
