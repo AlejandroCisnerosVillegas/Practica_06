4e76acc2c974510b96eabecce13e9205c2186819
Alejandro Cisneros Villegas <125778068+AlejandroCisnerosVillegas@users.noreply.github.com>
2023-03-06 13:20:03 -0600

    Add files via upload

---
+++ login.php
@@ -0,0 +1,29 @@
+<?php
+
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "general";
+
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+if (!$conn) 
+{
+	die("No hay conexión: ".mysqli_connect_error());
+}
+
+$nombre = $_POST["txtusuario"];
+$pass = $_POST["txtpassword"];
+
+$query = mysqli_query($conn,"SELECT * FROM poject_19_login WHERE usuario = '".$nombre."' and password = '".$pass."'");
+$nr = mysqli_num_rows($query);
+
+if($nr == 1)
+{
+	echo "Bienvenido:" .$nombre;
+}
+else if ($nr == 0) 
+{
+	echo "<script> alert('Error');window.location= 'index.html' </script>";
+}
+	
+?>
