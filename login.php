99b446cb5e0d14ea34bcebd59461ba8f4ff82b82
Cristian Alejandro Cisneros Villegas <125778068+AlejandroCisnerosVillegas@users.noreply.github.com>
2024-06-11 14:01:03 -0600

    Add files via upload

--- login.php
+++ login.php
@@ -1,29 +1,24 @@
 <?php
+	$dbhost = "localhost";
+	$dbuser = "root";
+	$dbpass = "";
+	$dbname = "general";
+	$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+	if (!$conn) 
+	{
+		die("No hay conexión: ".mysqli_connect_error());
+	}
+	$nombre = $_POST["txtusuario"];
+	$pass = $_POST["txtpassword"];
 
-$dbhost = "IP_Address";
-$dbuser = "User";
-$dbpass = "Password";
-$dbname = "DataBase_Name";
-
-$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
-if (!$conn) 
-{
-	die("No hay conexión: ".mysqli_connect_error());
-}
-
-$nombre = $_POST["txtusuario"];
-$pass = $_POST["txtpassword"];
-
-$query = mysqli_query($conn,"SELECT * FROM poject_19_login WHERE usuario = '".$nombre."' and password = '".$pass."'");
-$nr = mysqli_num_rows($query);
-
-if($nr == 1)
-{
-	echo "Bienvenido:" .$nombre;
-}
-else if ($nr == 0) 
-{
-	echo "<script> alert('Error');window.location= 'index.html' </script>";
-}
-	
+	$query = mysqli_query($conn,"SELECT * FROM poject_19_login WHERE usuario = '".$nombre."' and password = '".$pass."'");
+	$nr = mysqli_num_rows($query);
+	if($nr == 1)
+	{
+		echo "Bienvenido:" .$nombre;
+	}
+	else if ($nr == 0) 
+	{
+		echo "<script> alert('Error');window.location= 'index.html' </script>";
+	}	
 ?>
